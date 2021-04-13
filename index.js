50943ebab23902ce79eefb748f014f625c83eb4b
Oleksandra Shapovalova <shapovalovaova@gmail.com>
2021-04-13 12:03:00 +0200

    Saved file

--- index.js
+++ index.js
@@ -1,30 +1,5 @@
 'use strict';
 
-/*let activeUser = 'circle';
-
-const buttons = document.querySelectorAll('button');
-
-buttons.forEach((btn) =>
-  btn.addEventListener('click', () => {
-    if (
-      document.querySelector('p').innerHTML ==
-      'HRAJE: <img id="circle" src="imiges/circle.svg" alt="circle" />'
-    ) {
-      btn.style.class = 'board__field--circle';
-      btn.innerHTML =
-        '<img id="circle" src="imiges/circle.svg" alt="circle" />';
-      document.querySelector('p').innerHTML ==
-        'HRAJE: <img id="cross" src="imiges/cross.svg" alt="cross" />';
-    } else {
-      btn.style.class = 'board__field--cross';
-      btn.innerHTML = '<img id="cross" src="imiges/cross.svg" alt="cross" />';
-      document.querySelector('p').innerHTML ==
-        'HRAJE: <img id="circle" src="imiges/circle.svg" alt="circle" />';
-    }
-  }),
-);
-*/
-
 let activeUser = 'circle';
 const buttons = document.querySelectorAll('button');
 buttons.forEach((btn) =>
