3f9ad21ede146154052c56b2eae73056663caee8
Oleksandra Shapovalova <shapovalovaova@gmail.com>
2021-04-19 12:43:16 +0200

    Final version with animation

--- index.js
+++ index.js
@@ -5,17 +5,13 @@ const buttons = document.querySelectorAll('button');
 buttons.forEach((btn) =>
   btn.addEventListener('click', () => {
     if (activeUser == 'circle') {
-      btn.style.class = 'board__field--circle';
-      btn.innerHTML =
-        '<img style="color:black id="circle" src="imiges/circle.svg" alt="circle" />';
+      btn.classList.add('board__field--circle');
       document.querySelector('p').innerHTML =
         'HRAJE: <img id="cross" src="imiges/cross.svg" alt="cross" />';
       activeUser = 'cross';
       btn.disabled = true;
     } else {
-      btn.style.class = 'board__field--cross';
-      btn.innerHTML =
-        '<img style="color:black id="cross" src="imiges/cross.svg" alt="cross" />';
+      btn.classList.add('board__field--cross');
       document.querySelector('p').innerHTML =
         'HRAJE: <img id="circle" src="imiges/circle.svg" alt="circle" />';
       activeUser = 'circle';
