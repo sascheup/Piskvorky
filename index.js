92f17d1c09bbc4a4e62447986b8589d9578138d2
Oleksandra Shapovalova <shapovalovaova@gmail.com>
2021-04-24 18:07:07 +0200

    js

--- index.js
+++ index.js
@@ -1,21 +1,110 @@
 'use strict';
 
+const boardSize = 10;
+
+const getPosition = (field) => {
+  let fieldIndex = 0;
+  while (fieldIndex < fields.length) {
+    if (field === fields[fieldIndex]) {
+      break;
+    }
+    fieldIndex++;
+  }
+  return {
+    row: Math.floor(fieldIndex / boardSize),
+    column: fieldIndex / boardSize,
+  };
+};
+
+const getField = (row, column) => fields[row * boardSize + column];
+
+const getSymbol = (field) => {
+  if (field.classList.contains('board__field--cross')) {
+    return 'cross';
+  } else if (field.classList.contains('board__field--circle')) {
+    return 'circle';
+  }
+};
+const symbolsToWin = 5;
+const isWinningMove = (field) => {
+  console.log(field);
+  const origin = getPosition(field);
+  const symbol = getSymbol(field);
+
+  let i;
+
+  let inRow = 1; // Jednička pro právě vybrané políčko
+  // Koukni doleva
+  i = origin.column;
+  while (i > 0 && symbol === getSymbol(getField(origin.row, i - 1))) {
+    inRow++;
+    i--;
+  }
+
+  // Koukni doprava
+  i = origin.column;
+  while (
+    i < boardSize - 1 &&
+    symbol === getSymbol(getField(origin.row, i + 1))
+  ) {
+    inRow++;
+    i++;
+  }
+
+  if (inRow >= symbolsToWin) {
+    return true;
+  }
+
+  let inColumn = 1;
+  // Koukni nahoru
+  i = origin.row;
+  while (i > 0 && symbol === getSymbol(getField(i - 1, origin.column))) {
+    inColumn++;
+    i--;
+  }
+
+  // Koukni dolu
+  i = origin.row;
+  while (
+    i < boardSize - 1 &&
+    symbol === getSymbol(getField(i + 1, origin.column))
+  ) {
+    inColumn++;
+    i++;
+  }
+
+  if (inColumn >= symbolsToWin) {
+    return true;
+  }
+
+  return false;
+};
+
 let activeUser = 'circle';
-const buttons = document.querySelectorAll('button');
-buttons.forEach((btn) =>
-  btn.addEventListener('click', () => {
+const fields = document.querySelectorAll('button');
+
+fields.forEach((btn) =>
+  btn.addEventListener('click', (e) => {
     if (activeUser == 'circle') {
-      btn.classList.add('board__field--circle');
+      e.target.classList.add('board__field--circle');
       document.querySelector('p').innerHTML =
         'HRAJE: <img id="cross" src="imiges/cross.svg" alt="cross" />';
       activeUser = 'cross';
       btn.disabled = true;
     } else {
-      btn.classList.add('board__field--cross');
+      e.target.classList.add('board__field--cross');
       document.querySelector('p').innerHTML =
         'HRAJE: <img id="circle" src="imiges/circle.svg" alt="circle" />';
       activeUser = 'circle';
       btn.disabled = true;
+    }
+    let win = isWinningMove(btn);
+
+    if (win === true && activeUser === 'circle') {
+      alert(`Vyhrál kroužek.`);
+    }
+    if (win === true && activeUser === 'cross') {
+      alert(`Vyhrál křížek.`);
     }
   }),
 );
